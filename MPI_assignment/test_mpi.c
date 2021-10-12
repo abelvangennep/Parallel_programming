c38b4887f866d1279437e991db9308974c97443a
abelvangennep <54837851+abelvangennep@users.noreply.github.com>
2021-10-12 17:25:18 +0200

    Add files via upload

---
+++ MPI_assignment/test_mpi.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include "mpi.h"
+
+int N = 500;
+int R = 100;
+
+int test(int x) {
+	// Transform to a number beween 0 and 2 * pi.
+	double xd = ((double)x * (2 * M_PI) / N);
+
+	// Do a fixed amount of computation.
+	int n = 25000000;
+	for (int i = 0; i < n; i++) {
+		xd = sin(xd);
+		xd = asin(xd);
+	}
+	xd = sin(xd);
+
+	// Test the result.
+	return xd < -0.5;
+}
+
+int test_imbalanced(int x) {
+	// Transform to a number beween 0 and 2 * pi.
+	double xd = ((double)x * (2 * M_PI) / N);
+
+	// Do computation, the amount depends on the value of x.
+	int n = 100000 * x;
+
+	for (int i = 0; i < n; i++) {
+		xd = sin(xd);
+		xd = asin(xd);
+	}
+	xd = sin(xd);
+
+	// Test the result.
+	return xd < -0.5;
+}
+
+int *allocate_mem(int N) {
+	int *A = calloc(N, sizeof(int));
+	if (!A)
+		exit(0);
+	return A;
+}
+
+void fill_random(int *A, int N) {
+	srand(time(0));
+
+	for (int i = 0; i < N; i++) {
+		A[i] = (rand() % N);
+	}
+}
+
+void fill_ascending(int *A, int N) {
+	for (int i = 0; i < N; i++) {
+		A[i] = i;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
