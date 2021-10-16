b15a3a6b880df95a90ad9fd143206af2027b1d55
abelvangennep <54837851+abelvangennep@users.noreply.github.com>
2021-10-16 13:45:21 +0200

    Update test_sequential.c

--- MPI_assignment/test_sequential.c
+++ MPI_assignment/test_sequential.c
@@ -87,7 +87,7 @@ int main(int argc, char *argv[]) {
 	
 	for (int m = 0; m < N; m++) {
 		results += test(A[m]);
-		print("%d\n",results);
+		printf("%d\n",results);
 		
 		if (results > R){
 			printf("process is finished at itteration %d\n",m);
